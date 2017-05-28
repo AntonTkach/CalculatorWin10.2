75f5cfd14c549a397249d43b034ccfd89b59793a
Anton Tkach <dekerter@gmail.com>
2017-05-29 00:12:25 +0300

    Added main files and ClickHandler

---
+++ CalculatorWin10.2/ClickHandler.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorWin10._2
+{
+    class ClickHandler
+    {
+        public static void InformationPassed(string buttonValue)
+        {
+            int tempResult;
+            if (int.TryParse(buttonValue, out tempResult) |
+                buttonValue == ".")
+            {
+                //DisplayInfo.DisplayNumbers(buttonValue);
+            }
+            else
+            {
+                #region BigIfSentence
+                if (
+                    buttonValue == "memoryClear" ||
+                    buttonValue == "memoryRetrieve" ||
+                    buttonValue == "memoryAdd" ||
+                    buttonValue == "memorySubstract" ||
+                    buttonValue == "memorySave" ||
+                    buttonValue == "memory")
+                {
+                    //MemoryHandler.ActionMemory(buttonValue);
+                }
+                else if (buttonValue == "clear" ||
+                         buttonValue == "clearEverything" ||
+                         buttonValue == "erase")
+                {
+                    //DisplayInfo.InfoHandler(buttonValue);
+                }
+                else if (buttonValue == "percent" ||
+                         buttonValue == "squareRoot" ||
+                         buttonValue == "powerOfTwo" ||
+                         buttonValue == "oneOver" ||
+                         buttonValue == "addition" ||
+                         buttonValue == "subtraction" ||
+                         buttonValue == "multiplication" ||
+                         buttonValue == "division"
+                )
+                {
+                    //if (!DisplayInfo.IsFirstOperatorShown)
+                    //{
+                    //    MathControls.firstOperator = buttonValue;
+                    //    MathControls.WriteOperator(MathControls.firstOperator);
+                    //}
+                    //else
+                    //{
+                    //    MathControls.currentOperator = buttonValue;
+                    //    MathControls.WriteOperator(MathControls.currentOperator);
+                    //}
+
+
+                }
+                //else if (buttonValue == "equals")
+                //{
+                //    DisplayInfo.EqualHandler();
+                //}
+                //else if (buttonValue == "plusMinus")
+                //{
+                //    DisplayInfo.PlusMinus();
+                //}
+                #endregion
+            }
+        }
+    }
+
+}
--- CalculatorWin10.2/Standard.xaml.cs
+++ CalculatorWin10.2/Standard.xaml.cs
@@ -26,5 +26,27 @@ namespace CalculatorWin10._2
         {
             this.InitializeComponent();
         }
+
+        private static object myObj;
+        private void ButtonClicked(object sender, RoutedEventArgs e)
+        {
+            myObj = ((Button)sender).Tag;
+            ClickHandler.InformationPassed(myObj.ToString());
+            //expressionView.Text = DisplayInfo.currentExpression;
+            //screen.Text = DisplayInfo.ErrorOccured ?
+            //    "Cannot divide by zero" : DisplayInfo.ExpressionToSuitable(
+            //        DisplayInfo.expressionValue);
+            //DisplayInfo.ErrorOccured = false;
+
+        }
+        // OTHER FUNCTIONS
+        private void menu_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void history_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
